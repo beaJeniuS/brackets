55e677b1d98bff9613f034b6946472d1f44dae6b
beaJenius <beajenius@yandex.ru>
2022-01-24 23:49:04 +0300

    docs: job is done. Test with configs5-6 were very cool. It was nice joke...

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  let pairs = bracketsConfig.map((value) => {
+    return value[0]+value[1];
+  });
+  let resStr = '';
+  let testStr = str;
+  while(true){
+    resStr = testStr;
+    for(let i = 0;i < pairs.length; i++){
+      testStr = testStr.replace(pairs[i],'');
+    }
+
+    if(testStr.length === 0) {
+      return true;
+    }
+
+    if(resStr === testStr){
+      return false;
+    } else {
+      continue;
+    }
+  }
+} 
+
+  
